96c24ae5c7a6c7909d013b090dc96e08cd3d2084
samandar <samandar.abduhakimov.007@gmail.com>
2023-11-09 21:54:17 +0500

    g

--- String03.py
+++ String03.py
@@ -1,9 +1,2 @@
-def main():
-    """
-    "*" Return a string that consists of only this character and is five in length.
-    Args:
-        None
-    Returns:
-        str: return answer.
-    """
-    return 
+a="*****"
+print(len(a))
