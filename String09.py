c85d91877f6a80b7e3958d4db04bc2042ac13528
samandar <samandar.abduhakimov.007@gmail.com>
2023-11-09 22:09:39 +0500

    gg

--- String09.py
+++ String09.py
@@ -1,11 +1,4 @@
-def main(x1,x2,x3):
-    """
-    Given three integers, x1, x2, and x3, return the "[x1, x2, x3]" string.
-    Args:
-        x1: int
-        x2: int
-        x3: int
-    Returns:
-        str: return answer.
-    """
-    return 
+x1="[1 "
+x2="2 "
+x3="3]"
+print(x1+x2+x3)
