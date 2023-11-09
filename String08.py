ce3f7836b6e37d122360398c2b73620c8230a1c6
samandar <samandar.abduhakimov.007@gmail.com>
2023-11-09 22:10:23 +0500

    f

--- String08.py
+++ String08.py
@@ -1,10 +1,3 @@
-def main(first,last):
-    """
-    Given two strings, first_name and last_name, return a single string in the format "last, first".
-    Args:
-        first: str
-        last: str
-    Returns:
-        str: return answer.
-    """
-    return 
+fist='Otabek,'
+last=' Tursunov'
+print(fist+last)
